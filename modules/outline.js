d507420353ba7ed7298dbb8d7cac4ba055d6e682
Templin, Chris <ctemplin@gmail.com>
2021-04-19 09:02:32 -0400

    Moving outline into separate module.

---
+++ modules/outline.js
@@ -0,0 +1,74 @@
+import React,{useState} from "react"
+import ReactMarkdown from "react-markdown"
+import gfm from "remark-gfm"
+
+import outlineStyles from '../styles/Outline.module.css'
+
+class ItemGeneric extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+}
+
+class ItemGenericOutline extends ItemGeneric
+{
+  constructor(props) {
+    super(props) 
+    this.render = this.render.bind(this)
+    this.getClasses = this.getClasses.bind(this)
+    this.onChildToggle = this.onChildToggle.bind(this)
+    this.renderItem = this.renderItem.bind(this)
+    this.state = {showKids: false}
+  }
+
+  getClasses() {
+    var classNames = [outlineStyles.item]
+    switch (this.props.type) {
+      case "concept":
+      classNames.push(outlineStyles.concept)
+      break;
+      case "action":
+      classNames.push(outlineStyles.action);
+      break;
+      case "clarification":
+      classNames.push(outlineStyles.clarification);
+      break;
+
+      default:
+      break;
+    }
+    return classNames.join(' ')
+  }
+
+  onChildToggle(event) {
+    this.setState((state) => {
+      return {...state, showKids: !this.state.showKids}
+    })
+  }
+
+  renderItem(data) {
+    return (
+      <ItemGenericOutline {...data} />
+    )
+  }
+
+  render() {
+    return(
+    <div className={this.getClasses()} key={this.props.id}> 
+      <div>
+      {this.props.children ? <button className={outlineStyles.but} onClick={this.onChildToggle}>{!this.state.showKids ? "+" : "-" }</button> : ""} 
+      <span style={{textAlign: 'right', paddingRight: '1ex'}}>
+        {this.props.order}.
+      </span>
+      <span><ReactMarkdown remarkPlugin={{gfm}} className={outlineStyles.mdParent} children={this.props.text} /></span>
+      </div>
+      <div className={!this.state.showKids ? outlineStyles.childrenHide : outlineStyles.childrenShow}>
+      {this.props.children ? this.props.children.map(this.renderItem) : ""}
+      </div>
+    </div>
+    )
+  }
+  
+}
+
+export default ItemGenericOutline;
--- pages/index.js
+++ pages/index.js
@@ -1,73 +1,15 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import outlineStyles from '../styles/Outline.module.css'
+
 import React,{useState} from "react"
 import ReactMarkdown from "react-markdown"
 import gfm from "remark-gfm"
+import ItemGenericOutline from '../modules/outline'
 
 export async function getStaticProps() {
   return {
     props: {
     }
-  }
-}
-
-class ItemGeneric extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-}
-
-class ItemGenericOutline extends ItemGeneric {
-
-  constructor(props) {
-    super(props) 
-    this.render = this.render.bind(this)
-    this.getClasses = this.getClasses.bind(this)
-    this.onChildToggle = this.onChildToggle.bind(this)
-    this.state = {showKids: false}
-  }
-
-  getClasses() {
-    var classNames = [outlineStyles.item]
-    switch (this.props.type) {
-      case "concept":
-        classNames.push(outlineStyles.concept)
-        break;
-      case "action":
-        classNames.push(outlineStyles.action);
-        break;
-      case "clarification":
-        classNames.push(outlineStyles.clarification);
-        break;
-
-      default:
-        break;
-    }
-    return classNames.join(' ')
-  }
-
-  onChildToggle(event) {
-    this.setState((state) => {
-      return {...state, showKids: !this.state.showKids}
-    })
-  }
-
-  render() {
-    return(
-    <div className={this.getClasses()} key={this.props.id}> 
-      <div>
-        {this.props.children ? <button className={outlineStyles.but} onClick={this.onChildToggle}>{!this.state.showKids ? "+" : "-" }</button> : ""} 
-        <span style={{textAlign: 'right', paddingRight: '1ex'}}>
-          {this.props.order}.
-        </span>
-        <span><ReactMarkdown remarkPlugin={{gfm}} className={outlineStyles.mdParent} children={this.props.text} /></span>
-      </div>
-      <div className={!this.state.showKids ? outlineStyles.childrenHide : outlineStyles.childrenShow}>
-        {this.props.children ? this.props.children.map(renderItem) : ""}
-      </div>
-    </div>
-    )
   }
 }
 
